3317d7ae2b59d6cd1289ca2ab0af7bddc62dd194
nabeel584 <muhammadnabeel584@gmail.com>
2024-02-12 12:17:23 +0500

    updated code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -12,21 +13,39 @@ app.listen(3000, () => {
   console.log('server is listening for port 3000');
 });
 
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
+  const blogs = [
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+  ];
   //   res.send('<h1>Home pages</h1>');
-  res.render('index');
+  res.render('index', { title: 'Home', blogs });
 });
 app.get('/about', (req, res) => {
   //   res.send('<h1>About pages</h1>');
-  res.render('about');
+  res.render('about', { title: 'About' });
 });
 
 //redirect
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create');
+  res.render('create', { title: 'Create a new blog' });
 });
 
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { title: '404' });
 });
